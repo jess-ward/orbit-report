ffe88beb89729286aa2e39ffe64c7d12542df865
Jessica <jrnewport07@gmail.com>
2019-12-29 11:40:25 -0600

    fixed search button

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -11,7 +11,7 @@ export class OrbitListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.satellites)
+    // console.log(this.satellites)
   }
 
   sort(column: string): void {
